13a3f13a4eca5896f6e392297f1f490b9df0a070
gaspar frias <63258588+ThreshHG@users.noreply.github.com>
2022-04-27 05:57:45 -0300

    implementacion backend

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express')
 const fs = require('fs');
-const app = express()
+var app = express()
 const port = 3000
+app.use(express.static('./'));
+
 
 var files = fs.readdirSync("./carpetaDeEjemploALeer");
-const objeto = Object.assign({},files)
-// var myJsonString = JSON.stringify(objeto);
+var objeto = Object.assign({},files)
+//var res = objeto.replace("\"", "'");
+var myJsonString = JSON.stringify(objeto);
 
-app.get('/', (req, res) => {
-  res.json(objeto)
+app.get('/', function(req, res) {
+    res.render('./index.html');
+});
+
+app.get('/api', (req, res) => {
+  res.json(myJsonString)
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-const { Http2ServerRequest } = require('http2');
 
 //console.log(files);
 //console.log(objeto);
