4fde4016f206bb9221db1d2bd5f3a98e7073bae7
gab2408 <guerragabriel2408@gmail.com>
2022-04-27 16:32:51 -0300

    ultima modificacion

--- apache.sh
+++ apache.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
+apt install git -y
+git clone https://github.com/ThreshHG/AplicacionesLibres.git
+ip=`hostname -I |awk '{print $1}'`
 apache=`which apache2`
 o=`echo $?`
-
+apt install php
 if [ $o != 0  ]
 then
 apt update && apt upgrade -y
-apt install apache2 -y
-echo $disro 
+
 else 
 echo -e "Apache2 esta Instalado" 
 
 fi
-rm  /var/www/html/*
-unzip AplicacionesLibres.zip 
-sudo cp  AplicacionesLibres/* /var/www/html/
+
+systemctl start apache2
+
+php -S $ip:8080
+
+echo -e  "pueden acceder desde una navegador web con esta ip $ip:8080"
